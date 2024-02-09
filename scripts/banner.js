61fea7d0171b70d8e9c73a018e54f94e7e3befd1
Jacob Shaw <53603492+xGlacies@users.noreply.github.com>
2024-02-09 14:01:11 -0500

    Update banner.js

--- scripts/banner.js
+++ scripts/banner.js
@@ -10,14 +10,17 @@ function main()
             <a href=\"` + link_list.Course_List + `\" class=\"page_button center_text\">
                 Course<br>List
             </a>
+            <a href=\"` + link_list.Course_Description + `\" class=\"page_button center_text\">
+                Course<br>Description
+            </a>
             <a href=\"` + link_list.BSIT_Flowchart + `\" class=\"page_button center_text\">
                 BSIT<br>Flowchart
             </a>
             <a href=\"` + link_list.MSIT_Flowchart + `\" class=\"page_button center_text\">
                 MSIT<br>Flowchart
             </a>
-            <a href=\"` + link_list.Course_Description + `\" class=\"page_button center_text\">
-                Course<br>Description
+            <a href=\"` + link_list.Flex_Scheduler + `\" class=\"page_button center_text\">
+                Flex<br>Scheduler
             </a>
             <a href=\"` + link_list.Permanent_Schedule + `\" class=\"page_button center_text\">
                 Permanent<br>Schedule
