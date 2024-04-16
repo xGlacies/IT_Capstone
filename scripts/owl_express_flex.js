9148c19a9adc15949f8c0a8489a5e808f78c520e
Jacob Shaw <jakemarkshaw@gmail.com>
2024-04-16 18:41:59 -0400

    link search active

--- scripts/owl_express_flex.js
+++ scripts/owl_express_flex.js
@@ -1,24 +1,28 @@
 // Essential variables such as the URL query and elements on the page that will be referred to in the code
 var sidebar_list = document.getElementById("search_links");
 let front_link = "https://owlexpress.kennesaw.edu/prodban/bwckctlg.p_disp_listcrse?term_in=";
-let middle_link = "&subj_in=IT&crse_in=4983&schd_in=A";
+let subj_link = "&subj_in=";
+let crse_link = "&crse_in=";
+let end_link = "&schd_in=A";
+const current_sem = (sessionStorage.getItem("current_semester")).toString();
+const next_sem = (sessionStorage.getItem("next_semester")).toString();
+let link_current = front_link + current_sem + subj_link;
+let link_next = front_link + next_sem + subj_link;
 
 
 for (var i = 0; i < all_course_data.length; i++) {
+	
     var div = document.getElementById('search_links');
 
-
     var a = document.createElement('a');
-
-    a.href = "https://owlexpress.kennesaw.edu/prodban/bwckctlg.p_disp_listcrse?term_in=202401&subj_in=IT&crse_in=" + encodeURIComponent(all_course_data[i].Course_Number) + "&schd_in=A";
+	
+	a.href = link_current + encodeURIComponent(all_course_data[i].Prefix) + crse_link + encodeURIComponent(all_course_data[i].Course_Number) + end_link;
 
     a.target = "iframe_results";
 
-    a.textContent = all_course_data[i].Course_Number + " " + all_course_data[i].Course_Name;
-
+	a.textContent = all_course_data[i].Prefix + " " + all_course_data[i].Course_Number + ": " + all_course_data[i].Course_Name;
 
     div.appendChild(a);
-
 
 }
 
