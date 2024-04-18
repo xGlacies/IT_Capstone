9129354e37a62648c1eb7c1fa4f87a76aba10f4e
colezygaj <colezygaj@gmail.com>
2024-04-18 01:17:15 -0400

    Allow Search Owl Express to load from this page and not have to refresh

--- scripts/owl_express_flex.js
+++ scripts/owl_express_flex.js
@@ -1,3 +1,40 @@
+let lastSearchKeyword = '';
+
+
+
+const lastyear = (new Date().getFullYear() - 1).toString(); //get next year
+const year = new Date().getFullYear().toString(); //get current year
+const nextyear = (new Date().getFullYear() + 1).toString(); //get next year
+const month = new Date().getMonth()+1; //get current month
+let lastSemester = ""; //declared last semester as none to prepare for if statements
+let currentSemester = ""; //declared current semester as none to prepare for if statements
+let nextSemester = ""; //declared next semester as none to prepare for if statements
+let futureSemester = ""; //declared future semester as none to prepare for if statements
+
+if(month <= 4) {
+	lastSemester = lastyear + "08";
+	currentSemester = year + "01";
+	nextSemester = year + "05";
+	futureSemester = year + "08";
+}
+if(month >= 5 && month <= 7) {
+	lastSemester = year + "01";
+	currentSemester = year + "05";
+	nextSemester = year + "08";
+	futureSemester = nextyear + "01";
+}
+if(month >= 8) {
+	lastSemester = year + "05";
+	currentSemester = year + "08";
+	nextSemester = nextyear + "01";
+	futureSemester = nextyear + "05";
+}
+
+sessionStorage.setItem("last_semester", lastSemester)
+sessionStorage.setItem("current_semester", currentSemester)
+sessionStorage.setItem("next_semester", nextSemester)
+sessionStorage.setItem("future_semester", futureSemester)
+
 //Code below populates the semesters in the dropdown selection based on the current date/time
 const last_sem = (sessionStorage.getItem("last_semester")).toString();
 const current_sem = (sessionStorage.getItem("current_semester")).toString();
@@ -57,7 +94,6 @@ next_group.appendChild(future_option);
 //Code above populates the semesters in the dropdown selection based on the current date/time
 
 
-let lastSearchKeyword = '';
 
 // Function to update the course links based on the selected semester and course level
 function group_by_type(course_level, selectedSemester, filterKeyword = '') {
