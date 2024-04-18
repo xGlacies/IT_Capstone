cf7e7cab6cd8dc973e9d22977e4f238d2c7eccc6
Jacob Shaw <jakemarkshaw@gmail.com>
2024-04-18 05:17:38 -0400

    set default title method
    do not revert

--- scripts/owl_express_flex.js
+++ scripts/owl_express_flex.js
@@ -164,6 +164,10 @@ function set_title(semester, course) {
 	owl_express_flex_title.textContent = semester.toString() + " ~ " + course.toString();
 }
 
+function default_title() {
+	owl_express_flex_title.textContent = "Search Owl Express";;
+}
+
 // This only works if this file is loaded before the data_getter file.
 // MAKE SURE that this file is listed ABOVE the data_getter file in the script block.
 // The data_getter file has to have the SAME or lower load priority than this file. If this file is DEFER, data_getter MUST be DEFER.
